d08604d1f560999ab0b105b9ba7f45c2d3dc3754
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-10-03 12:20:08 -0500

    Inicializacion del Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'portafolio-balugrana';
+// }
+
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portafolio-balugrana';
+export class AppComponent implements OnInit {
+  title = 'mi-portafolio';
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: 'ease-in-out',
+      offset: 100
+    });
+  }
 }
